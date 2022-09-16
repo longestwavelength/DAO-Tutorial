4264c7be7154d824e7c0e9cd00827e979e8a834a
Nikhil <longestwavelength@protonmail.com>
2022-09-16 19:26:35 +0530

    Completed

---
+++ hardhat-tutorial/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x753F3A6264C75D2763e6fa8E1AfbAa545e7BA469";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
